54db41635d4a2fffdedc4abdafa641eca32a2377
Jonathan López <si727367@iteso.mx>
2021-01-26 07:29:23 -0600

    Clase 2.1

---
+++ Actividad 2/Variables_Avanzadas.java
@@ -0,0 +1,16 @@
+public class Variables_Avanzadas {
+    public static void main(String[] args) {
+        int N = 85;
+        double A = 8.36;
+        char C = 9812;
+
+        System.out.println("N = " + N);
+        System.out.println("A = " + A);
+        System.out.println("C = " + C);
+
+
+        System.out.println(N + " + " + A + " = " + (double) N+A);
+        System.out.println(A + " - " + N + " = " +  (A-(double)N));
+        System.out.println("Valor numerico del caracter C = " + (int)C);
+    }
+}
