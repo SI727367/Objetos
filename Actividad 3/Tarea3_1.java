660ec4580b4fa91c5e656fe9c698d2f38cef1088
Jonathan López <si727367@iteso.mx>
2021-01-27 12:25:22 -0600

    Actividad 3

---
+++ Actividad 3/Tarea3_1.java
@@ -0,0 +1,18 @@
+public class Tarea3_1 {
+    public static void main(String[] args) {
+        int N = 85;
+        double A = 8.36;
+        char C = 9812;
+
+        System.out.println("N = " + N);
+        System.out.println("A = " + A);
+        System.out.println("C = " + C);
+
+
+        System.out.println(N + " + " + A + " = " + (double) N+A);
+        System.out.println(A + " - " + N + " = " +  (A-(double)N));
+        System.out.println("Valor numerico del caracter C = " + (int)C);
+    }
+
+}
+
